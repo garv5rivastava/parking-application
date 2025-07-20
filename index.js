367e2a30647452f00ceeb6f36d74485239c22c5d
garv5rivastava <work.garv.is@gmail.com>
2025-07-20 18:38:46 +0530

    enabled cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const userRoutes = require('./routes/userRoutes');
@@ -8,6 +9,13 @@ require('dotenv').config();
 require('./db');
 const PORT = process.env.PORT;
 
+const corsOptions = {
+    origin: "http://127.0.0.1:5500/",
+    methods: "GET, POST, PUT, DEDLETE, PATCH, HEAD",
+    credentials: true,
+};
+
+app.use(cors(corsOptions)); 
 app.use(bodyParser.json());
 app.use('/users', userRoutes);
 app.use('/parkings', parkingRoutes);
